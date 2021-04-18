616361181af74138b97f3fddff738cd78b1d2b31
pavelprna <pavel@prna.ru>
2021-04-18 15:21:25 +0700

    feat: add script for edit profile

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,23 @@
 let popup = document.querySelector('.popup');
 let closePopupButton = popup.querySelector('.popup__close');
 let editProfileButton = document.querySelector('.profile__edit-button');
+let nameInput = popup.querySelector('.popup__input[name = nameInput]')
+let jobInput = popup.querySelector('.popup__input[name = jobInput]')
 
 
-function togglePopup(evt) {
-    evt.preventDefault();
+function togglePopup() {
     popup.classList.toggle('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    document.querySelector('.profile__title').textContent = nameInput.value;
+    document.querySelector('.profile__subtitle').textContent = jobInput.value;
+
+    togglePopup();
 }
 
 editProfileButton.addEventListener('click', togglePopup);
 closePopupButton.addEventListener('click', togglePopup);
+popup.addEventListener('submit', formSubmitHandler);
