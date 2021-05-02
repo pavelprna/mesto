e04f2c835820090a97f17d79ceb9d673c43016c4
pavelprna <pavel@prna.ru>
2021-05-02 22:47:28 +0700

    fix: create constants for repeating dom elements

--- scripts/script.js
+++ scripts/script.js
@@ -50,13 +50,17 @@ initialElements.forEach((element) => {
 
 function renderElement(element) {
     const elementItem = elementTemplate.querySelector('.elements__list-item').cloneNode(true);
-    elementItem.querySelector('.element__image').src = element.link;
-    elementItem.querySelector('.element__image').alt = element.name;
-    elementItem.querySelector('.element__title').textContent = element.name;
+    const elementImage = elementItem.querySelector('.element__image');
+    const elementTitle = elementItem.querySelector('.element__title');
+    elementImage.src = element.link;
+    elementImage.alt = element.name;
+    elementTitle.textContent = element.name;
+
     elementsList.prepend(elementItem);
+
     elementItem.querySelector('.element__like').addEventListener('click', toggleLike);
     elementItem.querySelector('.element__remove-button').addEventListener('click', removeCard);
-    elementItem.querySelector('.element__image').addEventListener('click', viewElementImage);
+    elementImage.addEventListener('click', viewElementImage);
 }
 
 function togglePopup(popupSelector) {
@@ -73,8 +77,9 @@ function removeCard(evt) {
 }
 
 function viewElementImage(evt) {
-    viewFullImagePopup.querySelector('.view-fullscreen__image').src = evt.target.src;
-    viewFullImagePopup.querySelector('.view-fullscreen__image').alt = evt.target.alt;
+    const fullScreenImage = viewFullImagePopup.querySelector('.view-fullscreen__image');
+    fullScreenImage.src = evt.target.src;
+    fullScreenImage.alt = evt.target.alt;
     viewFullImagePopup.querySelector('.view-fullscreen__caption').textContent = evt.target.alt;
 
     togglePopup('.popup_content_place-image');
