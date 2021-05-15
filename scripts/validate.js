f63256713be57bbbff4ef9c2278d1b6b0d4e6221
pavelprna <pavel@prna.ru>
2021-05-15 11:40:20 +0700

    feat: add validation code

---
+++ scripts/validate.js
@@ -0,0 +1,69 @@
+function showInputError (formElement, inputElement, errorMessage, config) {
+    const { inputErrorClass, errorClass } = config;
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(errorClass);
+}
+
+function hideInputError(formElement, inputElement, config) {
+    const { inputErrorClass, errorClass } = config;
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(inputErrorClass);
+    errorElement.classList.remove(errorClass);
+    errorElement.textContent = '';
+}
+
+function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+}
+
+function checkInputValidity(formElement, inputElement, config) {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, config);
+    } else {
+        hideInputError(formElement, inputElement, config);
+    }
+}
+
+function toggleButtonState(inputList, buttonElement, config) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add('form__submit-button_inactive');
+    } else {
+        buttonElement.classList.remove('button_inactive');
+    }
+}
+
+function setEventListeners(formElement, config) {
+    const { inputSelector, submitButtonSelector, ...restConfig } = config;
+    const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+    const buttonElement = formElement.querySelector(submitButtonSelector);
+    toggleButtonState(inputList, buttonElement, restConfig);
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, restConfig);
+            toggleButtonState(inputList, buttonElement, restConfig);
+        });
+    });
+}
+
+function enableValidation({ formSelector, ...restConfig }) {
+    const formList = Array.from(document.querySelectorAll(formSelector));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement, restConfig);
+    });
+}
+
+enableValidation({
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit-button',
+    inactiveButtonClass: 'form__submit-button_disabled',
+    inputErrorClass: 'form__input_type_error',
+    errorClass: 'form__input-error_visible'
+});
