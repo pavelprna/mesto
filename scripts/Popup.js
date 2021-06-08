720dd708d2788940278db42bc8f4c67de987d42b
pavelprna <pavel@prna.ru>
2021-06-08 17:09:56 +0700

    refactor: create PopupWithImage class

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,33 +1,31 @@
 export default class Popup {
-  #popup;
 
   constructor(elementSelector) {
-    this.#popup = document.querySelector(elementSelector);
-    this._popup = this.#popup;
+    this._popup = document.querySelector(elementSelector);
     this.setEventListeners();
   }
 
   open() {
-    document.addEventListener('keydown', this.#handleEscClose.bind(this));
-    this.#popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    this._popup.classList.add('popup_opened');
   }
 
   close() {
-    document.removeEventListener('keydown', this.#handleEscClose.bind(this));
-    this.#popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    this._popup.classList.remove('popup_opened');
   }
 
-  #handleEscClose(evt) {
+  _handleEscClose(evt) {
     if (evt.key === 'Escape') this.close();
   }
 
-  #handleOverlayClose(evt) {
+  _handleOverlayClose(evt) {
     if (evt.target === evt.currentTarget) this.close();
   }
 
   setEventListeners() {
-    const closePopupButton = this.#popup.querySelector('.popup__close')
-    closePopupButton.addEventListener('click', this.close);
-    this.#popup.addEventListener('mousedown', this.#handleOverlayClose);
+    const closePopupButton = this._popup.querySelector('.popup__close')
+    closePopupButton.addEventListener('click', this.close.bind(this));
+    this._popup.addEventListener('mousedown', this._handleOverlayClose.bind(this));
   }
 }
--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,23 +1,19 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  #image;
-  #alt;
-  #caption;
 
   constructor(data, elementSelector) {
     super(elementSelector);
-    this.#image = data.link;
-    this.#alt = data.alt;
-    this.#caption = data.caption;
+    this._image = data.link;
+    this._alt = data.alt;
+    this._caption = data.caption;
   }
 
   open() {
-    console.log(this._popup)
     const image = this._popup.querySelector('.view-fullscreen__image');
-    image.src = this.#image;
-    image.alt = this.#alt;
-    this._popup.querySelector('.view-fullscreen__caption').textContent = this.#caption;
+    image.src = this._image;
+    image.alt = this._alt;
+    this._popup.querySelector('.view-fullscreen__caption').textContent = this._caption;
     super.open();
   }
 }
