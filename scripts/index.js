512dd644bcef6119e4806cc29c69c4346067882c
pavelprna <pavel@prna.ru>
2021-06-11 23:07:49 +0700

    feat: add UserInfo class

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,12 @@ import Section from "./Section.js";
 import Card from "./Card.js";
 import PopupWithImage from "./PopupWithImage.js";
 import PopupWithForm from "./PopupWithForm.js";
+import UserInfo from "./UserInfo.js";
 
 const cardListSection = '.elements__list';
 const imagePopupSelector = '.popup_content_place-image';
 const profilePopupSelector = '.popup_content_edit-profile';
+const userInfoSelector = '.profile';
 
 // -------------------------------------- //
 
@@ -14,16 +16,16 @@ import FormValidator from "./FormValidator.js";
 
 const editProfileButton = document.querySelector('.profile__edit-button');
 const editProfileForm = document.querySelector('.form[name = edit-profile-form]')
-// const nameInput = editProfilePopup.querySelector('.form__input[name = name-input]');
-// const jobInput = editProfilePopup.querySelector('.form__input[name = job-input]');
-const name = document.querySelector('.profile__title');
-const job = document.querySelector('.profile__subtitle');
+const userNameSelector = '.profile__title';
+const userAboutSelector = '.profile__subtitle';
+const userNameInput = document.querySelector('.form__input[name = name]')
+const userAboutInput = document.querySelector('.form__input[name = about]')
 
 const addNewCardButton = document.querySelector('.profile__add-button');
-const newCardPopup = document.querySelector('.popup_content_new-card');
+const newCardPopupSelector = '.popup_content_new-card';
 const newCardForm = document.querySelector('.form[name = add-card-form]')
-const placeNameInput = newCardPopup.querySelector('.form__input[name = place-name-input]');
-const placeLinkInput = newCardPopup.querySelector('.form__input[name = place-link-input]');
+const placeNameInput = document.querySelector('.form__input[name = place-name-input]');
+const placeLinkInput = document.querySelector('.form__input[name = place-link-input]');
 
 // enable validation for all forms:
 const editProfileValidator = new FormValidator(formConfig, editProfileForm);
@@ -31,6 +33,8 @@ editProfileValidator.enableValidation();
 const addNewCardValidator = new FormValidator(formConfig, newCardForm);
 addNewCardValidator.enableValidation();
 
+
+// render card list:
 const cardList = new Section({
   items: initialElements,
   renderer: (item) => {
@@ -46,8 +50,33 @@ const cardList = new Section({
 
 cardList.renderItems();
 
+// user info:
+const userInfo = new UserInfo(userNameSelector, userAboutSelector);
 
 
+// profile popup:
+const profilePopup = new PopupWithForm(profilePopupSelector, (inputValues) => {
+  userInfo.setUserInfo(inputValues);
+
+  profilePopup.close();
+});
+
+profilePopup.setEventListeners();
+
+editProfileButton.addEventListener('click', () => {
+  const { name, about } = userInfo.getUserInfo();
+
+  userNameInput.value = name;
+  userAboutInput.value = about;
+
+  editProfileValidator.clearInputErrors();
+  profilePopup.open();
+});
+
+// new card popup:
+const newCardPopup = new PopupWithForm(newCardPopupSelector, (inputValues) => {
+  const card = new Card(inputValues, '#card-element')
+})
 function handleNewCardSubmit(evt) {
   evt.preventDefault();
   renderCard({
@@ -57,30 +86,11 @@ function handleNewCardSubmit(evt) {
   closePopup(newCardPopup);
 }
 
-const profilePopup = new PopupWithForm(profilePopupSelector, (inputValues) => {
-  const { name, job } = inputValues;
-
-  // TODO: send data to UserInfo
-
-  profilePopup.close();
-});
-
-profilePopup.setEventListeners();
-
-editProfileButton.addEventListener('click', () => {
-
-  // TODO: accept data from UserInfo
-
-  editProfileValidator.clearInputErrors();
-  profilePopup.open();
-});
-
-
 addNewCardButton.addEventListener('click', () => {
   newCardForm.reset();
   addNewCardValidator.clearInputErrors();
   openPopup(newCardPopup);
 });
 
-newCardPopup.addEventListener('submit', handleNewCardSubmit);
+// newCardPopup.addEventListener('submit', handleNewCardSubmit);
 
